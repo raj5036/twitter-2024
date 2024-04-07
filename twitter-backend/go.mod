098f51c6518f343acc7effb340c6b6f275779d08
Raj Karmakar <97rajmath@gmail.com>
2024-04-07 13:01:25 +0530

    Installed Mux Router & Initial setups

--- twitter-backend/go.mod
+++ twitter-backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/raj5036/twitter-2024
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- twitter-backend/main.go
+++ twitter-backend/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/raj5036/twitter-2024/router"
 )
 
 func main() {
 	fmt.Println("Twitter Server is running successfully")
 
 	// Setup routers
+	router := router.Router()
+	log.Fatal(http.ListenAndServe(":4000", router))
+	fmt.Println("Listening on PORT: 4000...........")
+
 	// Setup MongoDB
 }
