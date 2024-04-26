5119ffcaa1306731fcc821b33ce01ba85d92f027
Raj Karmakar <97rajmath@gmail.com>
2024-04-26 12:20:30 +0530

    Added BSON entries for user model

--- twitter-backend/model/models.go
+++ twitter-backend/model/models.go
@@ -4,11 +4,11 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name        string             `json:"name,omitempty"`
-	PhoneNumber string             `json:"phoneNumber,omitempty"`
-	Email       string             `json:"email,omitempty"`
-	DOB         string             `json:"dob,omitempty"`
-	Tweets      any                `json:"tweets,omitempty"`
-	Likes       any                `json:"likes,omitempty"`
-	Replies     any                `json:"replies,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	PhoneNumber string             `json:"phoneNumber,omitempty" bson:"phoneNumber,omitempty"`
+	Email       string             `json:"email,omitempty" bson:"email,omitempty"`
+	DOB         string             `json:"dob,omitempty" bson:"dob,omitempty"`
+	Tweets      any                `json:"tweets,omitempty" bson:"tweets,omitempty"`
+	Likes       any                `json:"likes,omitempty" bson:"likes,omitempty"`
+	Replies     any                `json:"replies,omitempty" bson:"replies,omitempty"`
 }
