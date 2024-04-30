982507c16137810fd93064ca15824918a336bbf1
Raj Karmakar <97rajmath@gmail.com>
2024-04-30 12:56:33 +0530

    Created API routes for Tweet controllers

---
+++ twitter-backend/controller/tweetController.go
@@ -0,0 +1,19 @@
+package controller
+
+import "net/http"
+
+func CreateTweet(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetTweetsByUserId(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateTweet(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTweet(w http.ResponseWriter, r *http.Request) {
+
+}
--- twitter-backend/router/router.go
+++ twitter-backend/router/router.go
@@ -4,17 +4,22 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	userController "github.com/raj5036/twitter-2024/controller"
+	controller "github.com/raj5036/twitter-2024/controller"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/user/registration", userController.RegisterUser).Methods(http.MethodPost)
-	router.HandleFunc("/user/login", userController.LoginUser).Methods(http.MethodPost)
+	// User Routes
+	router.HandleFunc("/user/registration", controller.RegisterUser).Methods(http.MethodPost)
+	router.HandleFunc("/user/login", controller.LoginUser).Methods(http.MethodPost)
+	router.HandleFunc("/user/delete-all", controller.DeleteAllUser).Methods(http.MethodDelete)
 
-	// Utility APIs
-	router.HandleFunc("/user/delete-all", userController.DeleteAllUser).Methods(http.MethodDelete)
+	// Tweet routes
+	router.HandleFunc("/tweet/create", controller.CreateTweet).Methods(http.MethodPost)
+	router.HandleFunc("/tweet/get/:userId", controller.GetTweetsByUserId).Methods(http.MethodGet)
+	router.HandleFunc("/tweet/update/:id", controller.UpdateTweet).Methods(http.MethodPatch)
+	router.HandleFunc("/tweet/delete/:id", controller.DeleteTweet).Methods(http.MethodDelete)
 
 	return router
 }
